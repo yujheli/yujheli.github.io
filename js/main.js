7ef5020c9ea54377ec2975b5266f26852326083d
yujheli <yujhe5413@ntu.edu.tw>
2018-03-07 15:25:50 +0800

    update personal web

--- js/main.js
+++ js/main.js
@@ -1,149 +1,145 @@
-;(function () {
-	
-	'use strict';
+(function ($) {
 
-	var isMobile = {
-		Android: function() {
-			return navigator.userAgent.match(/Android/i);
-		},
-			BlackBerry: function() {
-			return navigator.userAgent.match(/BlackBerry/i);
-		},
-			iOS: function() {
-			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-		},
-			Opera: function() {
-			return navigator.userAgent.match(/Opera Mini/i);
-		},
-			Windows: function() {
-			return navigator.userAgent.match(/IEMobile/i);
-		},
-			any: function() {
-			return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-		}
-	};
-
-	
-	var fullHeight = function() {
-
-		if ( !isMobile.any() ) {
-			$('.js-fullheight').css('height', $(window).height());
-			$(window).resize(function(){
-				$('.js-fullheight').css('height', $(window).height());
-			});
-		}
-	};
-
-	// Parallax
-	var parallax = function() {
-		$(window).stellar();
-	};
-
-	var contentWayPoint = function() {
-		var i = 0;
-		$('.animate-box').waypoint( function( direction ) {
-
-			if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
-				
-				i++;
-
-				$(this.element).addClass('item-animate');
-				setTimeout(function(){
-
-					$('body .animate-box.item-animate').each(function(k){
-						var el = $(this);
-						setTimeout( function () {
-							var effect = el.data('animate-effect');
-							if ( effect === 'fadeIn') {
-								el.addClass('fadeIn animated-fast');
-							} else if ( effect === 'fadeInLeft') {
-								el.addClass('fadeInLeft animated-fast');
-							} else if ( effect === 'fadeInRight') {
-								el.addClass('fadeInRight animated-fast');
-							} else {
-								el.addClass('fadeInUp animated-fast');
-							}
-
-							el.removeClass('item-animate');
-						},  k * 100, 'easeInOutExpo' );
-					});
-					
-				}, 50);
-				
-			}
-
-		} , { offset: '85%' } );
-	};
-
-
-
-	var goToTop = function() {
-
-		$('.js-gotop').on('click', function(event){
-			
-			event.preventDefault();
-
-			$('html, body').animate({
-				scrollTop: $('html').offset().top
-			}, 500, 'easeInOutExpo');
-			
-			return false;
-		});
-
-		$(window).scroll(function(){
-
-			var $win = $(window);
-			if ($win.scrollTop() > 200) {
-				$('.js-top').addClass('active');
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollup').fadeIn();
 			} else {
-				$('.js-top').removeClass('active');
+				$('.scrollup').fadeOut();
 			}
-
+		});
+		$('.scrollup').click(function(){
+			$("html, body").animate({ scrollTop: 0 }, 1000);
+				return false;
 		});
 	
-	};
+	// local scroll
+	jQuery('.navbar').localScroll({hash:true, offset: {top: 0},duration: 800, easing:'easeInOutExpo'});
 
-	var pieChart = function() {
-		$('.chart').easyPieChart({
-			scaleColor: false,
-			lineWidth: 4,
-			lineCap: 'butt',
-			barColor: '#FF9000',
-			trackColor:	"#f5f5f5",
-			size: 160,
-			animate: 1000
-		});
-	};
+	
+	// portfolio
+    if($('.isotopeWrapper').length){
 
-	var skillsWayPoint = function() {
-		if ($('#fh5co-skills').length > 0 ) {
-			$('#fh5co-skills').waypoint( function( direction ) {
-										
-				if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-					setTimeout( pieChart , 400);					
-					$(this.element).addClass('animated');
+        var $container = $('.isotopeWrapper');
+        var $resize = $('.isotopeWrapper').attr('id');
+        // initialize isotope
+        
+        $container.isotope({
+            itemSelector: '.isotopeItem',
+            resizable: false, // disable normal resizing
+            masonry: {
+                columnWidth: $container.width() / $resize
+            }
+
+
+            
+        });
+
+        $('#filter a').click(function(){
+
+
+
+            $('#filter a').removeClass('current');
+            $(this).addClass('current');
+            var selector = $(this).attr('data-filter');
+            $container.isotope({
+                filter: selector,
+                animationOptions: {
+                    duration: 1000,
+                    easing: 'easeOutQuart',
+                    queue: false
+                }
+            });
+            return false;
+        });
+        
+        
+        $(window).smartresize(function(){
+            $container.isotope({
+                // update columnWidth to a percentage of container width
+                masonry: {
+                    columnWidth: $container.width() / $resize
+                }
+            });
+        });
+        
+
+}  
+
+
+	// fancybox
+	jQuery(".fancybox").fancybox();
+
+
+	if (Modernizr.mq("screen and (max-width:1024px)")) {
+			jQuery("body").toggleClass("body");
+			
+	} else {
+		var s = skrollr.init({
+			mobileDeceleration: 1,
+			edgeStrategy: 'set',
+			forceHeight: true,
+			smoothScrolling: true,
+			smoothScrollingDuration: 300,
+				easing: {
+					WTF: Math.random,
+					inverted: function(p) {
+						return 1-p;
+					}
 				}
-			} , { offset: '90%' } );
-		}
-
-	};
+			});	
+	}
 
 
-	// Loading page
-	var loaderPage = function() {
-		$(".fh5co-loader").fadeOut("slow");
-	};
 
-	
-	$(function(){
-		contentWayPoint();
-		goToTop();
-		loaderPage();
-		fullHeight();
-		parallax();
-		// pieChart();
-		skillsWayPoint();
-	});
+	//scroll menu
+	jQuery('.appear').appear();
+	jQuery(".appear").on("appear", function(data) {
+			var id = $(this).attr("id");
+			jQuery('.nav li').removeClass('active');
+			jQuery(".nav a[href='#" + id + "']").parent().addClass("active");					
+		});
 
 
-}());
+		//parallax
+        var isMobile = false;
+
+        if(Modernizr.mq('only all and (max-width: 1024px)') ) {
+            isMobile = true;
+        }
+
+        
+        if (isMobile == false && ($('#parallax1').length  ||isMobile == false &&  $('#parallax2').length ||isMobile == false &&  $('#testimonials').length))
+        {
+
+
+            $(window).stellar({
+                responsive:true,
+                scrollProperty: 'scroll',
+                parallaxElements: false,
+                horizontalScrolling: false,
+                horizontalOffset: 0,
+                verticalOffset: 0
+            });
+
+        }
+    
+    //Google Map
+    var get_latitude = $('#google-map').data('latitude');
+    var get_longitude = $('#google-map').data('longitude');
+
+    function initialize_google_map() {
+        var myLatlng = new google.maps.LatLng(get_latitude, get_longitude);
+        var mapOptions = {
+            zoom: 14,
+            scrollwheel: false,
+            center: myLatlng
+        };
+        var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+        var marker = new google.maps.Marker({
+            position: myLatlng,
+            map: map
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initialize_google_map);
+
+})(jQuery);
